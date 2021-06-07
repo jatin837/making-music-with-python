7a880fb471b09a21c9fc1e7830ec197add13d5b0
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-07 23:05:06 +0530

    syntax error

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-import numpy import array, sin
+from numpy import array, sin
 
 def main():
     STEP: float = 0.01
     SAMPLING_RATE: float = 48000
-    WAVE: np.array = np.linspace(1.0, SAMPLING_RATE, STEP)
-     
+    WAVE: array = np.sin(np.linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
+    print(WAVE)
     
 
 if __name__ = "__main__":
