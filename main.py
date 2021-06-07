afe0cb862e4f6571159674f85c87788a1633e96f
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-07 23:44:02 +0530

    still some problem with the way it's evaluated

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     OUTPUT: str = os.path.abspath("./output.bin")
     SAMPLING_RATE: float = 48000
     CMD: str = f'ffplay -f f32le -showmode 1  -ar {SAMPLING_RATE} {OUTPUT}'
-    STEP: float = 0.01
+    STEP: float = 0.02
     WAVE: array = sin(VOLUME*linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
 
     with open(OUTPUT, 'wb') as f:
