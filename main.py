0ae53c4ac9115c2b55d105dff54911de86f11fd8
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-07 23:31:35 +0530

    Yeah it works, fish shell has some trouble with configuring with conda environ

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
-from numpy import array, sin
+from numpy import array, sin, linspace
 
 def main():
+
+    OUTPUT: str = os.path.abspath("./output.bin")
     STEP: float = 0.01
     SAMPLING_RATE: float = 48000
-    WAVE: array = np.sin(np.linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
-    print(WAVE)
+    WAVE: array = sin(linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
+
+    with open(OUTPUT, 'wb') as f:
+        f.write(WAVE)
     
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
