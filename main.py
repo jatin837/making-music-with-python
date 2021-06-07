5dee7cb90657e51facbd22e295b859ac894a1b74
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-07 22:52:56 +0530

    declare wave and step variable

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
-import numpy as np
+import numpy import array, sin
 
 def main():
-    pass
+    STEP: float = 0.01
+    SAMPLING_RATE: float = 48000
+    WAVE: np.array = np.linspace(1.0, SAMPLING_RATE, STEP)
+     
     
+
 if __name__ = "__main__":
     main()
