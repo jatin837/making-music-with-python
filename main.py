9bd4a8fd51cf53722e2731d0992c412c7d3770f9
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-07 23:35:42 +0530

    ADD command in python script

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from numpy import array, sin, linspace
 def main():
 
     OUTPUT: str = os.path.abspath("./output.bin")
-    STEP: float = 0.01
     SAMPLING_RATE: float = 48000
+    CMD: str = f'ffplay -f -showmod 1 f32le -ar {SAMPLING_RATE} {OUTPUT}'
+    STEP: float = 0.01
     WAVE: array = sin(linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
 
     with open(OUTPUT, 'wb') as f:
         f.write(WAVE)
+
     
 
 if __name__ == "__main__":
