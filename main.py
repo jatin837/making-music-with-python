26199324933cbecf7d943f0ac1fa1585af0d3341
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-07 23:49:50 +0530

    add duration variable to better control duration of sound file

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import os
 from numpy import array, sin, linspace
 
 def main():
-
+    DURATION:float = 4
     VOLUME:float = 2
     OUTPUT: str = os.path.abspath("./output.bin")
     SAMPLING_RATE: float = 48000
-    CMD: str = f'ffplay -f f32le -showmode 1  -ar {SAMPLING_RATE} {OUTPUT}'
-    STEP: float = 0.02
-    WAVE: array = sin(VOLUME*linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
+    CMD: str = f'ffplay -f f32le -showmode 1 -ar {SAMPLING_RATE} {OUTPUT}'
+    STEP: float = 0.01
+    WAVE: array = sin(VOLUME*linspace(0.0, SAMPLING_RATE*STEP*4, 4*SAMPLING_RATE))
 
     with open(OUTPUT, 'wb') as f:
         f.write(WAVE)
 
-    os.system(CMD)
+    os.system(CMD + "&")
     
 
 if __name__ == "__main__":
