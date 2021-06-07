c49c4308a5dcd3b466ac8c59bba8baf28b978d74
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-07 23:40:51 +0530

    still attrocious sound, adjusted volume

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from numpy import array, sin, linspace
 
 def main():
 
+    VOLUME:float = 2
     OUTPUT: str = os.path.abspath("./output.bin")
     SAMPLING_RATE: float = 48000
     CMD: str = f'ffplay -f f32le -showmode 1  -ar {SAMPLING_RATE} {OUTPUT}'
     STEP: float = 0.01
-    WAVE: array = sin(linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
+    WAVE: array = sin(VOLUME*linspace(0.0, SAMPLING_RATE*STEP, SAMPLING_RATE))
 
     with open(OUTPUT, 'wb') as f:
         f.write(WAVE)
