671320e3f04b562e8356c140436694efd84abcd3
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-08 16:54:58 +0530

    STATUS:DONE ===========   now you can control this using frequency, sampling rate and duration   of sound

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from math import pi, sin
 import struct
 
 def generate_sound(duration: float, volume: float, sampling_rate:float, freq: float):
-    wave: array = array([sin(freq*i) for i in arange(0, sampling_rate*duration)])
+    wave: array = array([sin(i) for i in linspace(0, 2*pi*freq*duration, duration*sampling_rate)])
     return wave
 
 def main():
     ################################# Variable Declaration ##############################
-    DURATION:float = 1 
-    FREQ: float = 0.05
+    DURATION:float = 3 
+    FREQ: float = 440
     VOLUME:float = 4
     LOG:str = os.path.abspath("./debug.log")
     OUTPUT: str = os.path.abspath("./output.bin")
