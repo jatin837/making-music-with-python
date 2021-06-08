eeeda291bd00170f2e5cbea5d762d5f56742793a
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-08 15:19:27 +0530

    FAILUR not the desired result

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import os
 from numpy import array, sin, linspace
+from math import pi
 import struct
 
-def generate_sound(duration: float, volume: float, sampling_rate: float, step:float):
-    wave: array = sin(volume*linspace(0.0, sampling_rate*step*duration, int(sampling_rate*duration)))
+def generate_sound(duration: float, volume: float, sampling_rate: float, freq: float):
+    wave: array = volume*sin((freq/(2*pi))*linspace(0.0, duration, int(sampling_rate*duration)))
     return wave
 
 def main():
     ################################# Variable Declaration ##############################
     DURATION:float = 2 
+    FREQ: float = 440
     VOLUME:float = 2
     LOG:str = os.path.abspath("./debug.log")
     OUTPUT: str = os.path.abspath("./output.bin")
-    SAMPLING_RATE: float = 48000 # number of samples per seconds from output.bin
+    SAMPLING_RATE: float = FREQ # number of samples per seconds from output.bin
     CMD: str = f'ffplay -f f32le -showmode 1 -ar {SAMPLING_RATE} {OUTPUT}'
     STEP: float = 0.01
     ############################# Variable Declaration ends #############################
-    WAVE:array = generate_sound(duration = DURATION, volume = VOLUME, sampling_rate = SAMPLING_RATE, step = STEP)
+    WAVE:array = generate_sound(duration = DURATION, volume = VOLUME, sampling_rate = SAMPLING_RATE, freq = FREQ)
 
     bytestring = ''
     for wave in WAVE:
